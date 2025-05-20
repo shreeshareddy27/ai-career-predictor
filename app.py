1a261005ca9c2918a72aba5aab0edb9c1832b12e
shreeshareddy27 <119696549+shreeshareddy27@users.noreply.github.com>
2025-05-20 01:15:51 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 import pickle
 
 # Load tokenizer and model
-model = load_model('career_model.h5')
+model = load_model("career_model.keras")
 with open('tokenizer.pkl', 'rb') as f:
     tokenizer = pickle.load(f)
 
